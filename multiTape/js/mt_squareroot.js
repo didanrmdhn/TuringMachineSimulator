38ec1646dab198adfc4c261b7b77d885bdbc6cd6
anangfath <anangmuhammad245@gmail.com>
2023-07-01 18:51:06 +0700

    Final Code

--- multiTape/js/mt_squareroot.js
+++ multiTape/js/mt_squareroot.js
@@ -1,76 +1,60 @@
 var nval1 = document.getElementById("val1");
-var nval2 = document.getElementById("val2");
 var turingVal = [];
 var turingVal2 = [];
 var turingVal3 = [];
 var turingVal4 = [];
+var turingVal5 = [];
 var i;
 var j;
 var k;
 var l;
 var m;
+var n;
 var result = 0;
 var nowState = 0;
+var acceptingState = 5;
 var activeState = document.getElementsByClassName("state-active");
 var trTape = document.getElementById("turing_tape");
 var trTape2 = document.getElementById("turing_tape2");
 var trTape3 = document.getElementById("turing_tape3");
 var trTape4 = document.getElementById("turing_tape4");
+var trTape5 = document.getElementById("turing_tape5");
 var dispState = document.getElementById("show-state-now");
 var dispAnswer = document.getElementById("show-answer");
 var stepController = document.getElementById("controller_step");
 var initController = document.getElementById("init_step");
-var skipController = document.getElementById("controller_skip");
-var acceptingState = 17;
 
+//sama kaya mtr tapi 3 tape
 function init() {
   trDelete();
 
-  if (nval1.value >= 0 && nval2.value >= 0) {
+  if (nval1.value > 0) {
     for (i = 0; i < 2; i++) {
       turingVal.push(new createState("B"));
       turingVal2.push(new createState2("B"));
       turingVal3.push(new createState3("B"));
       turingVal4.push(new createState4("B"));
-    }
-    if (nval1.value > 0) {
-      turingVal.push(new createState("X"));
-      turingVal2.push(new createState2("B"));
-      turingVal3.push(new createState3("B"));
-      turingVal4.push(new createState4("B"));
+      turingVal5.push(new createState5("B"));
     }
     for (i = 0; i < nval1.value; i++) {
       turingVal.push(new createState("0"));
       turingVal2.push(new createState2("B"));
       turingVal3.push(new createState3("B"));
       turingVal4.push(new createState4("B"));
-    }
-    turingVal.push(new createState("1"));
-    turingVal2.push(new createState2("B"));
-    turingVal3.push(new createState3("B"));
-    turingVal4.push(new createState4("B"));
-    if (nval2.value > 0) {
-      turingVal.push(new createState("X"));
-      turingVal2.push(new createState2("B"));
-      turingVal3.push(new createState3("B"));
-      turingVal4.push(new createState4("B"));
-    }
-    for (i = 0; i < nval2.value; i++) {
-      turingVal.push(new createState("0"));
-      turingVal2.push(new createState2("B"));
-      turingVal3.push(new createState3("B"));
-      turingVal4.push(new createState4("B"));
+      turingVal5.push(new createState5("B"));
     }
     for (i = 0; i < 2; i++) {
       turingVal.push(new createState("B"));
       turingVal2.push(new createState2("B"));
       turingVal3.push(new createState3("B"));
       turingVal4.push(new createState4("B"));
+      turingVal5.push(new createState5("B"));
     }
     trTape.childNodes[2].classList.add("state-active");
     trTape2.childNodes[2].classList.add("state-active");
     trTape3.childNodes[2].classList.add("state-active");
     trTape4.childNodes[2].classList.add("state-active");
+    trTape5.childNodes[2].classList.add("state-active");
   } else {
     alert("Nilai belum dimasukan/negatif");
   }
@@ -78,9 +62,8 @@ function init() {
   k = 2;
   l = 2;
   m = 2;
+  n = 2;
   state = 0;
-
-  document.getElementById("controller_skip").addEventListener("click", skipController);
 }
 
 function createState(val) {
@@ -123,6 +106,17 @@ function createState4(val4) {
     this.val = newValue;
   };
 }
+function createState5(val5) {
+  this.val = val5;
+  var item5 = document.createElement("p");
+  var n5 = document.createTextNode(this.val);
+  item5.appendChild(n5);
+  trTape5.appendChild(item5);
+  this.replaceWith = function (newValue) {
+    this.val = newValue;
+  };
+}
+
 function createMostLeftNode(val) {
   this.val = val;
   var item = document.createElement("p");
@@ -133,6 +127,7 @@ function createMostLeftNode(val) {
     this.val = newValue;
   };
 }
+
 function createMostLeftNode2(val2) {
   this.val = val2;
   var item2 = document.createElement("p");
@@ -143,6 +138,7 @@ function createMostLeftNode2(val2) {
     this.val = newValue;
   };
 }
+
 function createMostLeftNode3(val3) {
   this.val = val3;
   var item3 = document.createElement("p");
@@ -163,11 +159,21 @@ function createMostLeftNode4(val4) {
     this.val = newValue;
   };
 }
+function createMostLeftNode5(val5) {
+  this.val = val5;
+  var item5 = document.createElement("p");
+  var n5 = document.createTextNode(this.val);
+  item5.appendChild(n5);
+  trTape5.insertBefore(item5, trTape5.firstChild);
+  this.replaceWith = function (newValue) {
+    this.val = newValue;
+  };
+}
 
 function displayAnswer() {
-  for (i = 0; i < turingVal4.length; i++) {
-    console.log(turingVal4.length);
-    if (turingVal4[i].val == "0") {
+  for (i = 0; i < turingVal5.length; i++) {
+    console.log(turingVal5.length);
+    if (turingVal5[i].val == "0") {
       result++;
     }
   }
@@ -185,10 +191,12 @@ function trDelete() {
   trTape2.innerHTML = "";
   trTape3.innerHTML = "";
   trTape4.innerHTML = "";
+  trTape5.innerHTML = "";
   turingVal = [];
   turingVal2 = [];
   turingVal3 = [];
   turingVal4 = [];
+  turingVal5 = [];
   result = 0;
   stepController.disabled = false;
 }
@@ -200,53 +208,35 @@ function step() {
       activeState[i].classList.remove("state-active");
       activeState[i].classList.remove("state-active");
       activeState[i].classList.remove("state-active");
+      activeState[i].classList.remove("state-active");
     }
   }
-
   finished = 0;
-
   //nowState,oldvalue1,oldvalue2 | nextState,newvalue1,newvalue2,dir1,dir2,dir3,addblank
-  go(0, "X", "B", "B", "B", 1, "B", "B", "B", "B", "R", "S", "S", "S");
-  go(0, "1", "B", "B", "B", 15, "B", "B", "B", "X", "S", "S", "S", "R");
-  go(1, "0", "B", "B", "B", 1, "B", "0", "0", "0", "R", "R", "R", "R");
-  go(1, "1", "B", "B", "B", 2, "B", "B", "B", "B", "R", "S", "S", "S");
-  go(2, "X", "B", "B", "B", 3, "B", "B", "B", "B", "R", "S", "L", "S");
-  go(2, "B", "B", "B", "B", 16, "B", "B", "B", "B", "S", "S", "S", "L");
-  go(3, "0", "0", "0", "B", 3, "0", "0", "0", "0", "S", "S", "L", "R");
-  go(3, "0", "0", "B", "B", 4, "0", "0", "B", "B", "S", "R", "R", "S");
-  go(3, "0", "B", "0", "B", 5, "B", "B", "0", "B", "R", "S", "S", "S");
-  go(4, "0", "0", "0", "B", 4, "0", "0", "0", "0", "S", "S", "R", "R");
-  go(4, "0", "0", "B", "B", 3, "0", "0", "B", "B", "S", "R", "L", "S");
-  go(4, "0", "B", "0", "B", 6, "B", "B", "0", "B", "R", "S", "S", "S");
-  go(5, "0", "B", "0", "B", 5, "0", "B", "B", "B", "S", "S", "L", "S");
-  go(5, "B", "B", "0", "B", 17, "B", "B", "0", "B", "S", "S", "S", "S");
-  go(5, "0", "B", "B", "B", 7, "0", "B", "B", "B", "S", "S", "S", "L");
-  go(6, "0", "B", "0", "B", 6, "0", "B", "B", "B", "S", "S", "R", "S");
-  go(6, "B", "B", "0", "B", 17, "B", "B", "0", "B", "S", "S", "S", "S");
-  go(6, "0", "B", "B", "B", 8, "0", "B", "B", "B", "S", "S", "S", "L");
-  go(7, "0", "B", "B", "0", 7, "0", "B", "0", "B", "S", "S", "R", "L");
-  go(7, "0", "B", "B", "B", 9, "0", "B", "B", "B", "S", "L", "L", "S");
-  go(8, "0", "B", "B", "0", 8, "0", "B", "0", "B", "S", "S", "L", "L");
-  go(8, "0", "B", "B", "B", 10, "0", "B", "B", "B", "S", "L", "R", "S");
-  go(9, "0", "0", "0", "B", 9, "0", "0", "0", "0", "S", "S", "L", "R");
-  go(9, "0", "B", "0", "B", 11, "B", "B", "0", "B", "R", "S", "S", "S");
-  go(9, "0", "0", "B", "B", 10, "0", "0", "B", "B", "S", "L", "R", "S");
-  go(10, "0", "0", "0", "B", 10, "0", "0", "0", "0", "S", "S", "R", "R");
-  go(10, "0", "B", "0", "B", 12, "B", "B", "0", "B", "R", "S", "S", "S");
-  go(10, "0", "0", "B", "B", 9, "0", "0", "B", "B", "S", "L", "L", "S");
-  go(11, "0", "B", "0", "B", 11, "0", "B", "B", "B", "S", "S", "L", "S");
-  go(11, "0", "B", "B", "B", 13, "0", "B", "B", "B", "S", "S", "S", "L");
-  go(11, "B", "B", "0", "B", 17, "B", "B", "0", "B", "S", "S", "S", "S");
-  go(12, "0", "B", "0", "B", 12, "0", "B", "B", "B", "S", "S", "R", "S");
-  go(12, "0", "B", "B", "B", 14, "0", "B", "B", "B", "S", "S", "S", "L");
-  go(12, "B", "B", "0", "B", 17, "B", "B", "0", "B", "S", "S", "S", "S");
-  go(13, "0", "B", "B", "0", 13, "0", "B", "0", "B", "S", "S", "R", "L");
-  go(13, "0", "B", "B", "B", 3, "0", "B", "B", "B", "S", "R", "L", "S");
-  go(14, "0", "B", "B", "0", 14, "0", "B", "0", "B", "S", "S", "L", "L");
-  go(14, "0", "B", "B", "B", 4, "0", "B", "B", "B", "S", "R", "R", "S");
-  go(15, "B", "B", "B", "B", 17, "B", "B", "B", "X", "S", "S", "S", "R");
-  go(16, "B", "B", "B", "B", 15, "B", "B", "B", "0", "S", "S", "S", "L");
-  go(16, "B", "B", "B", "0", 16, "B", "B", "B", "B", "S", "S", "S", "L");
+  go(0, "0", "B", "B", "B", "B", 1, "0", "0", "0", "B", "0", "S", "S", "S", "S", "R", 0);
+  go(1, "0", "0", "0", "B", "B", 1, "0", "0", "0", "0", "B", "S", "S", "L", "R", "S", 0);
+  go(1, "0", "0", "B", "B", "B", 2, "0", "0", "B", "B", "B", "S", "L", "R", "S", "S", 0);
+  go(1, "0", "B", "0", "B", "B", 7, "0", "B", "0", "B", "B", "S", "R", "S", "S", "S", 0);
+  go(2, "0", "0", "B", "B", "B", 1, "0", "0", "B", "B", "B", "S", "L", "L", "S", "S", 0);
+  go(2, "0", "0", "0", "B", "B", 2, "0", "0", "0", "0", "B", "S", "S", "R", "R", "S", 0);
+  go(2, "0", "B", "0", "B", "B", 3, "0", "B", "0", "B", "B", "S", "R", "S", "S", "S", 0);
+  go(3, "0", "0", "0", "B", "B", 3, "0", "0", "0", "B", "B", "S", "R", "R", "S", "S", 0);
+  go(3, "0", "B", "B", "B", "B", 4, "0", "B", "B", "B", "B", "S", "L", "L", "L", "S", 0);
+  go(4, "0", "0", "0", "0", "B", 4, "0", "0", "0", "B", "B", "R", "S", "S", "L", "S", 0);
+  go(4, "B", "0", "0", "B", "B", 5, "B", "0", "0", "B", "B", "S", "S", "S", "S", "S", 0);
+  go(4, "0", "0", "0", "B", "B", 6, "0", "0", "0", "B", "B", "L", "S", "S", "R", "S", 0);
+  go(4, "B", "0", "0", "0", "B", 8, "B", "0", "0", "0", "B", "L", "S", "S", "S", "L", 0);
+  go(6, "B", "0", "0", "B", "B", 0, "B", "0", "0", "B", "B", "R", "R", "R", "S", "S", 0);
+  go(6, "0", "0", "0", "B", "B", 6, "0", "0", "0", "B", "B", "L", "S", "S", "S", "S", 0);
+  go(7, "0", "0", "0", "B", "B", 7, "0", "0", "0", "B", "B", "S", "R", "S", "S", "S", 0);
+  go(7, "0", "B", "0", "B", "B", 4, "0", "B", "0", "B", "B", "S", "L", "S", "L", "S", 0);
+  go(8, "0", "0", "0", "0", "0", 5, "0", "0", "0", "0", "B", "S", "S", "S", "S", "S", 0);
+
+  console.log("turingVal:", turingVal);
+  console.log("turingVal2:", turingVal2);
+  console.log("turingVal3:", turingVal3);
+  console.log("turingVal4:", turingVal4);
+  console.log("turingVal5:", turingVal5);
 
   if (state == acceptingState) {
     displayState("Selesai");
@@ -262,24 +252,30 @@ function skipState() {
   }
 }
 
-function go(nowState, oldVal, oldVal2, oldVal3, oldVal4, nextState, newVal, newVal2, newVal3, newVal4, directiont1, directiont2, directiont3, directiont4) {
-  if (turingVal[j].val == oldVal && turingVal2[k].val == oldVal2 && turingVal3[l].val == oldVal3 && turingVal4[m].val == oldVal4 && state == nowState && finished == 0) {
+function go(nowState, oldVal, oldVal2, oldVal3, oldVal4, oldVal5, nextState, newVal, newVal2, newVal3, newVal4, newVal5, directiont1, directiont2, directiont3, directiont4, directiont5, addBlank) {
+  if (turingVal[j].val == oldVal && turingVal2[k].val == oldVal2 && turingVal3[l].val == oldVal3 && turingVal4[m].val == oldVal4 && turingVal5[n].val == oldVal5 && state == nowState && finished == 0) {
     turingVal[j].replaceWith(newVal);
     turingVal2[k].replaceWith(newVal2);
     turingVal3[l].replaceWith(newVal3);
     turingVal4[m].replaceWith(newVal4);
+    turingVal5[n].replaceWith(newVal5);
+
     trTape.childNodes[j].textContent = newVal;
     trTape2.childNodes[k].textContent = newVal2;
     trTape3.childNodes[l].textContent = newVal3;
     trTape4.childNodes[m].textContent = newVal4;
+    trTape5.childNodes[n].textContent = newVal5;
     trTape.childNodes[j].classList.add("state-active");
     trTape2.childNodes[k].classList.add("state-active");
     trTape3.childNodes[l].classList.add("state-active");
     trTape4.childNodes[m].classList.add("state-active");
+    trTape5.childNodes[n].classList.add("state-active");
     trTape.childNodes[j].scrollIntoView();
     trTape2.childNodes[k].scrollIntoView();
     trTape3.childNodes[l].scrollIntoView();
     trTape4.childNodes[m].scrollIntoView();
+    trTape5.childNodes[n].scrollIntoView();
+
     state = nextState;
     displayState(
       "(q" +
@@ -294,14 +290,18 @@ function go(nowState, oldVal, oldVal2, oldVal3, oldVal4, nextState, newVal, newV
         oldVal3 +
         "," +
         oldVal4 +
+        "," +
+        oldVal5 +
         "/" +
         newVal +
         "," +
         newVal2 +
         "," +
         newVal3 +
-        ", " +
+        "," +
         newVal4 +
+        "," +
+        newVal5 +
         ", " +
         directiont1 +
         "," +
@@ -309,29 +309,37 @@ function go(nowState, oldVal, oldVal2, oldVal3, oldVal4, nextState, newVal, newV
         "," +
         directiont3 +
         "," +
-        directiont4
+        directiont4 +
+        "," +
+        directiont5
     );
-    if (j == turingVal.length - 1 || k == turingVal2.length - 1 || l == turingVal3.length - 1 || m == turingVal4.length - 1) {
+    if (j == turingVal.length - 1 || k == turingVal2.length - 1 || l == turingVal3.length - 1 || m == turingVal4.length - 1 || n == turingVal5.length - 1) {
       turingVal.push(new createState("B"));
       turingVal2.push(new createState2("B"));
       turingVal3.push(new createState3("B"));
       turingVal4.push(new createState4("B"));
+      turingVal5.push(new createState5("B"));
     }
-    if (j == 0 || k == 0 || l == 0 || m == 0) {
+
+    if (j == 0 || k == 0 || l == 0) {
       turingVal.unshift(new createMostLeftNode("B"));
       turingVal2.unshift(new createMostLeftNode2("B"));
       turingVal3.unshift(new createMostLeftNode3("B"));
       turingVal4.unshift(new createMostLeftNode4("B"));
-      j++;
-      k++;
-      l++;
-      m++;
+      turingVal5.unshift(new createMostLeftNode5("B"));
+
+      j += 1;
+      k += 1;
+      l += 1;
+      m += 1;
+      n += 1;
     }
-    decide(directiont1, directiont2, directiont3, directiont4);
+
+    decide(directiont1, directiont2, directiont3, directiont4, directiont5);
     finished = 1;
   }
 }
-function decide(dt1, dt2, dt3, dt4) {
+function decide(dt1, dt2, dt3, dt4, dt5) {
   //mungkin ganti switch case
   if (dt1 == "R") {
     j++;
@@ -360,5 +368,12 @@ function decide(dt1, dt2, dt3, dt4) {
     m--;
   } else {
     m;
+  }
+  if (dt5 == "R") {
+    n++;
+  } else if (dt5 == "L") {
+    n--;
+  } else {
+    n;
   }
 }
